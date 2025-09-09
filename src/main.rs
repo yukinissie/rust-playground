881135b5a2794298b85044c3e56c4655a237b831
yukinissie <bigtree2490@outlook.jp>
2025-09-09 20:20:27 +0900

    feat: ✨ Add procedural macro `HelloMacro` and related packages
    * Implemented the `HelloMacro` procedural macro in the `hello_macro_derive` crate.
    * Added dependencies for `quote` and `syn` in the respective `Cargo.toml` files.
    * Updated the main application to utilize the `HelloMacro` derive macro.
    * Created `hello_macro` and `hello_macro_derive` crates with necessary configurations.

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,18 @@ macro_rules! vec {
     };
 }
 
+// 手続き型マクロ（カスタムderiveマクロ）
+use hello_macro::HelloMacro;
+use hello_macro_derive::HelloMacro;
+
+#[derive(HelloMacro)]
+struct Pancakes;
+
 fn main() {
     // 宣言型マクロ
     let v: Vec<u32> = vec![1, 2, 3];
     println!("{:?}", v);
+
+    // 手続き型マクロ（カスタムderiveマクロ）
+    Pancakes::hello_macro();
 }
