d63d6dc3083caadf55021ba5dd49c9614ff2776f
yukinissie <bigtree2490@outlook.jp>
2025-09-09 19:44:05 +0900

    feat: ✨ Implement declaration macro `vec!`
    - Added a declaration macro `vec!` to create a vector from a list of expressions.
    - Updated `main` function to demonstrate usage of the new macro.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+// 宣言型マクロ
+#[macro_export]
+macro_rules! vec {
+    ( $( $x:expr ),* ) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    // 宣言型マクロ
+    let v: Vec<u32> = vec![1, 2, 3];
+    println!("{:?}", v);
 }
